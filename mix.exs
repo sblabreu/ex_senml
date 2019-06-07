afa1a1ccf85b8a3a962b813226089ab0613e0e84
sblabreu <sblabreu@gmail.com>
2019-06-08 00:14:25 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule ExSenml.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:ex_doc, "~> 0.19.1", only: :docs},
+      {:ex_doc, "~> 0.19.1", only: :dev},
       {:jason, "~> 1.1"}
     ]
   end
